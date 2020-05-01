238667822b1690d8cdf449516f2d37e33b6d449d
Fascodd <45606128+Fascodd@users.noreply.github.com>
2020-05-01 10:51:07 -0700

    Shape Object Selector update
    Added shape classes
    Added method to create shape (child of canvas-wrapper)

---
+++ math/src/presentationSpace.js
@@ -0,0 +1,48 @@
+import React from 'react';
+
+class PresentationSpace extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            shapeArray: [{ id: 1, classes: "shape", img: "parallelogram", shape: "parallelogram" },
+            { id: 2, classes: "shape", img: "square", shape: "square" },
+            { id: 3, classes: "shape", img: "rectangle", shape: "rectangle" },
+            { id: 4, classes: "shape", img: "circle", shape: "circle" },
+            { id: 5, classes: "shape", img: "triangle", shape: "triangle-up" },
+            ],
+            shapeStorage: [],
+
+        }
+        this.addShape = this.addShape.bind(this);
+    }
+    addShape(e) {
+        let canvas = document.getElementById("drawing-space-wrapper");
+        let div = document.createElement("div");
+        div.classList = e.target.id;
+        canvas.appendChild(div);
+        div.addEventListener("mousedown", ()=>console.log("mousedown"))
+    }
+    render() {
+
+        return (
+            <div id="presentation-space" >
+                <div id="object-wrapper">
+                    <div id="object-selector">
+                        {/* have the hright of classes by ceiling of totalNum/2 */}
+                        {this.state.shapeArray.map(item =>
+                            <div key={item.id} className={item.classes} id={item.shape}
+                                onClick={this.addShape}>{item.img}</div>
+                        )}
+                    </div>
+                    <div id="object-selector-fill" />
+                </div>
+                <div id="drawing-space-wrapper">
+                    <div id="drawing-space">
+
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+export default PresentationSpace;
